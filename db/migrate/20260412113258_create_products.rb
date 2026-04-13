09db80dfdb3ca1ce1af80a24c3fa68dcdb399c44
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2026-04-13 09:18:10 +0900

    fix:typo

--- db/migrate/20260412113258_create_products.rb
+++ db/migrate/20260412113258_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[8.0]
   def change
     create_table :products do |t|
       t.string :name
-      t.text :definition
+      t.text :description
       
       t.timestamps
     end
